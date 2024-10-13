a81bfa05405c4be6a659d9e4f77ce8bc211354ec
Vsevolod Epinetov <vsevolod@epinetov.com>
2024-10-13 14:49:20 +0300

    fix: moved the statement 1 line below

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,9 @@ function isPalindrome (string) {
   const adaptedString = string.toLowerCase().replaceAll(' ', '');
 
   for (let i = 0; i < adaptedString.length / 2; i++) {
-    if (adaptedString[i] !== adaptedString[adaptedString.length - i - 1]) { return false; }
+    if (adaptedString[i] !== adaptedString[adaptedString.length - i - 1]) {
+      return false;
+    }
   }
 
   return true;
