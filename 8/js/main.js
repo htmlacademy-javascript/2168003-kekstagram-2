d9bf6a11ff2f0b832c9d25341a53c7f2a9d001d1
Keks <keks@htmlacademy.ru>
2024-12-25 03:46:43 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,8 @@
+import { displayPosts } from './display-posts.js';
+import { getPosts } from './get-posts.js';
+import { lightroom } from './lightroom.js';
+import { AMOUNT_OF_POSTS } from './settings.js';
+
+const posts = getPosts(AMOUNT_OF_POSTS);
+displayPosts(posts);
+lightroom(posts);
