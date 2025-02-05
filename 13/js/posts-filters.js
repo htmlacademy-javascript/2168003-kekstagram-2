68e65865517a34ce407624e88aa8136b0540ff03
Keks <keks@htmlacademy.ru>
2025-02-05 11:45:48 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/posts-filters.js
@@ -0,0 +1,37 @@
+import { displayPosts } from './display-posts.js';
+import { AMOUNT_OF_RANDOM_POSTS, RERENDER_DELAY } from './settings.js';
+import { debounce, shuffleArray } from './util.js';
+
+export function postFilters(posts) {
+  const buttons = document.querySelectorAll('.img-filters__button');
+
+  const renderFilteredPosts = (filterType) => {
+    let postsCopy = posts.slice();
+
+    switch (filterType) {
+      case 'random':
+        postsCopy = shuffleArray(postsCopy).slice(0, AMOUNT_OF_RANDOM_POSTS);
+        break;
+      case 'discussed':
+        postsCopy.sort((prev, next) => next.comments.length - prev.comments.length);
+        break;
+      default:
+        break;
+    }
+    displayPosts(postsCopy);
+  };
+  const debouncedRenderPosts = debounce(renderFilteredPosts, RERENDER_DELAY);
+
+  buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+      const currentButton = document.querySelector('.img-filters__button--active');
+      currentButton.classList.remove('img-filters__button--active');
+      button.classList.add('img-filters__button--active');
+
+      const filterType = button.id.split('filter-')[1];
+
+      debouncedRenderPosts(filterType);
+    });
+  });
+}
---
+++ 13/js/toasts.js
@@ -0,0 +1,41 @@
+import { MILISECONDS_IN_SECOND } from './settings.js';
+
+export function showToast(templateId, mainClass, { onClose = () => {}, closeAfter = null } = {}) {
+  const toastTemplate = document.querySelector(`#${templateId}`).content.querySelector(`.${mainClass}`);
+  const toast = toastTemplate.cloneNode(true);
+  const toastButton = toast.querySelector('button');
+  const bodyElement = document.querySelector('body');
+
+  const onEscButtonPress = (evt) => {
+    if (evt.key === 'Escape') {
+      closeToast();
+    }
+  };
+
+  function closeToast() {
+    toast.remove();
+    document.removeEventListener('keydown', onEscButtonPress);
+    onClose();
+  }
+
+  bodyElement.insertAdjacentElement('beforeend', toast);
+
+  if (closeAfter) {
+    setTimeout(() => {
+      closeToast();
+    }, closeAfter * MILISECONDS_IN_SECOND);
+    return;
+  }
+
+  toastButton.addEventListener('click', () => {
+    closeToast();
+  });
+
+  toast.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains(mainClass)) {
+      closeToast();
+    }
+  });
+
+  document.addEventListener('keydown', onEscButtonPress);
+}
